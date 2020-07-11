47b561de851dce9f91c33f32da8b70a98370f501
Jesse <jesse10klein@gmail.com>
2020-07-11 23:35:29 +1000

    can now use own files or spotify songs

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,4 @@
-
+let playing = false;
 let audio = null;
 let timeoutSet = false;
 const barLengthSlider = document.getElementById('barLengthSlider');
@@ -9,8 +9,18 @@ window.onload = function() {
   audio.crossOrigin = 'anonymous';
   audio.volume = 0.1;
   const canvas = document.getElementById('canvas');
+  const file = document.getElementById('file');
+  file.onchange = function() {
+    if (!playing) {
+      startPlaying(URL.createObjectURL(this.files[0]));
+      playing = true;
+    } else {
+      audio.src = URL.createObjectURL(this.files[0]);
+      audio.load();
+      audio.play();
+    }
+  }
 };
-
 
 function startPlaying(source) {
   audio.src = source;
@@ -32,14 +42,13 @@ function startPlaying(source) {
   analyser.fftSize = 2048;
 
   var bufferLength = analyser.frequencyBinCount;
-  console.log(bufferLength);
 
   var dataArray = new Uint8Array(bufferLength);
 
   var WIDTH = canvas.width;
   var HEIGHT = canvas.height;
 
-  const angleMultiplier = (12 * Math.PI) / (bufferLength);
+  const angleMultiplier = (21 * Math.PI) / (bufferLength);
 
   var barWidth = (WIDTH / bufferLength) * 1.5;
   var barHeight;
@@ -81,12 +90,6 @@ function startPlaying(source) {
       colour = "rgb(" + r + "," + g + "," + b + ")";
       drawRotatedRectangle(ctx, (i * angleMultiplier), barHeight, colour, innerRadius);
 
-      //tdrawCircles++;
-      //if (tdrawCircles > 20) {
-      //  drawCircles(ctx, circles, canvasCentre);
-      //  tdrawCircles = 0;
-      //}
-
 
       x += barWidth + 1;
     }
@@ -98,19 +101,6 @@ function startPlaying(source) {
       b = 0;
       bIncrement = 4;
     }
-    if (pastTotal != null) {
-      if ((total - pastTotal) > 8000) {
-        console.log("HERE");
-        if (circles.length < maxCircles) {
-          if (circles.length == 0 || circles[circles.length - 1].radius > 50) {
-            circles.push({radius: 0, colour: "rgba(255,255,255,1)"});
-          }
-          
-        }
-      }
-    }
-
-    pastTotal = total;
   }
   audio.play();
   renderFrame();
@@ -131,32 +121,6 @@ function drawRotatedRectangle(ctx, angle, magnitude, colour, innerRadius) {
   ctx.stroke();
 }
 
-function drawCircles(ctx, circles, canvasCentre) {
-  for (let i = 0; i < circles.length; i++) {
-    if (circles[i].radius > 400) {
-      circles.splice(circles.indexOf(circles[i]), 1);
-      return;
-    }
-    if (circles[i].radius > 250) {
-      alpha = 1 - ((circles[i].radius - 250) / 100);
-      circles[i].colour = `rgba(255, 255, 255, ${alpha})`;
-    }
-    if (circles[i].radius < 250) {
-      blue = 100;
-      red = (255 - circles[i].radius) / 2 + 100;
-      green = (255 - circles[i].radius) /2 + 100;
-      circles[i].colour = `rgba(${red}, ${green}, ${blue}, 1)`;
-    }
-    ctx.lineWidth = 1;
-    ctx.strokeStyle = circles[i].colour;
-    ctx.beginPath();
-    ctx.arc(canvasCentre.x, canvasCentre.y, circles[i].radius, 0, 2 * Math.PI);
-    ctx.stroke();
-    circles[i].radius += 0.15;
-  }
-  
-}
-
 function formatAutocompleteTag(tag) {
   const HTML = `
     <div class='search-tag'>
@@ -174,7 +138,14 @@ function processPreview(elem) {
   //Hide all boxed;
   const dropdown = $("#search-dropdown");
   dropdown.empty();
-  startPlaying(url);
+  if (!playing) {
+    startPlaying(url);
+    playing = true;
+  } else {
+    audio.src = url;
+    audio.load();
+    audio.play();
+  }
 }
 
 function updateAutoComplete(matches) {
@@ -217,7 +188,6 @@ $("#search-term").on('keyup', function () {
   $.ajax({
     url, type: "POST", data,
     success: function(response) {
-      console.log(response);
       updateAutoComplete(response);
     }
   })
